661aaca5d06ef7667cad1e46354fe84bec581f9b
PERERALAB <kandarpbioinfo@gmail.com>
2025-01-15 12:58:25 -0500

    include nanocount

--- nanopore_illumina_compare.R
+++ nanopore_illumina_compare.R
@@ -83,16 +83,61 @@ ggplot(na.omit(inmat),
   geom_point() + 
   geom_smooth(method="lm",color="red")
 
-tab<-read.table("7316-1700.hg38.counts.tsv", header = T, sep = "\t")
-tab$transcript_name<-gsub("\\..*", "", tab$transcript_name)
-
-cmerge<-merge(tab[,c("transcript_name", "raw")], pbta.in, by.x="transcript_name", by.y=0)
-cmerge<-cmerge[,c("transcript_name", "raw", "7316-1700")]
-
-ggplot(cmerge, 
-       aes(raw, `7316-1700`)) +
-  geom_point() + 
-  geom_smooth(method="lm",color="red")
+nc_files<-list.files(pattern = "hg38.NanoCount.tsv")
+nc_list<-lapply(nc_files,function (x) read.table(x, header = T, sep = "\t"))
+nc_mat<-Reduce(function(x,y) merge(x,y, by="transcript_name", all=T), nc_list)
+nc_mat<-nc_mat[,c(1, grep("tpm", colnames(nc_mat)))]
+nc_mat[is.na(nc_mat)]<-0
+nc_snames<-gsub(".hg38.NanoCount.tsv", "", nc_files)
+colnames(nc_mat)<-c("transcriipt_name", nc_snames)
+nc_mat$transcriipt_name<-gsub("\\..*", "", nc_mat$transcriipt_name)
+rownames(nc_mat)<-nc_mat$transcriipt_name
+nc_mat<-nc_mat[,-1]
+counts_df3 <- calculate_gene_counts(nc_mat)
+# Combine counts into a table
+result_table <- data.frame(
+DataFrame = c(rep("Illumina", length(counts_df1)),
+rep("lr-kallisto", length(counts_df2)), #,
+rep("NanoCount", length(counts_df3))),
+Column = c(names(counts_df1), names(counts_df2), names(counts_df3)),
+Count = c(counts_df1, counts_df2, counts_df3)
+)
+# Print the table
+print(result_table)
+cmerge<-merge(nc_mat[,c("7316-1700")], pbta.in, by=0)
+head (cmerge)
+cmerge<-merge(nc_mat, pbta.in, by=0)
+cmerge
+cmerge<-cmerge[,c("Row.names", "7316-1700.x", "7316-1700.y")]
+cmerge<-merge(cmerge, com.in, by.x="Row.names", by.y=0)
+head (cmerge)
+cmerge<-cmerge[,c(1,2,3,6)]
+rownames(cmerge)<-cmerge$Row.names
+cmerge<-cmerge[,-1]
+colnames(cmerge)<-c("NanoCount", "Illumina", "lr-kallisto")
+cmerge<-log2(na.omit(cmerge[rowSums(cmerge>0.1)==3,]))
+corres<-rcorr(as.matrix(cmerge))
+cor_mat<-corres$r
+p_mat<-corres$P
+corrplot(
+cor_mat,
+method = "number",
+type = "lower",
+p.mat = p_mat,
+pch.cex = 1.2,
+pch.col = "darkred",
+mar = c(0,0,2,0)
+)
+ggplot(cmerge,
+aes(`lr-kallisto`, Illumina)) +
+geom_point() +
+geom_smooth(method="lm",color="red")
+ggplot(cmerge,
+aes(NanoCount, Illumina)) +
+geom_point() +
+geom_smooth(method="lm",color="red")
+cor.test(cmerge$Illumina, cmerge$NanoCount)
+cor.test(cmerge$Illumina, cmerge$`lr-kallisto`)
 
 
 
