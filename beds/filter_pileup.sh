2ed9040ceee3af3e34b303e0bd685d4e35fbe5df
kandarpRJ <kandarpbioinfo@gmail.com>
2025-01-19 11:55:29 -0500

    filter pileups

---
+++ beds/filter_pileup.sh
@@ -0,0 +1,60 @@
+gzcat 7316-1700.pileup.bed.gz | awk '{if ($4=="a" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-1700.pileup.a.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-1700.pileup.bed.gz | awk '{if ($4=="p" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-1700.pileup.p.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-1700.pileup.bed.gz | awk '{if ($4=="m" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-1700.pileup.m.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-1700.pileup.bed.gz | awk '{if ($4=="i" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-1700.pileup.i.cov4_pmod10_mod2_omod0.bed
+gzcat 7316-2227.pileup.bed.gz | awk '{if ($4=="a" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-2227.pileup.a.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-2227.pileup.bed.gz | awk '{if ($4=="p" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-2227.pileup.p.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-2227.pileup.bed.gz | awk '{if ($4=="m" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-2227.pileup.m.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-2227.pileup.bed.gz | awk '{if ($4=="i" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-2227.pileup.i.cov4_pmod10_mod2_omod0.bed
+gzcat 7316-2252.pileup.bed.gz | awk '{if ($4=="a" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-2252.pileup.a.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-2252.pileup.bed.gz | awk '{if ($4=="p" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-2252.pileup.p.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-2252.pileup.bed.gz | awk '{if ($4=="m" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-2252.pileup.m.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-2252.pileup.bed.gz | awk '{if ($4=="i" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-2252.pileup.i.cov4_pmod10_mod2_omod0.bed
+gzcat 7316-2322.pileup.bed.gz | awk '{if ($4=="a" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-2322.pileup.a.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-2322.pileup.bed.gz | awk '{if ($4=="p" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-2322.pileup.p.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-2322.pileup.bed.gz | awk '{if ($4=="m" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-2322.pileup.m.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-2322.pileup.bed.gz | awk '{if ($4=="i" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-2322.pileup.i.cov4_pmod10_mod2_omod0.bed
+gzcat 7316-2700.pileup.bed.gz | awk '{if ($4=="a" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-2700.pileup.a.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-2700.pileup.bed.gz | awk '{if ($4=="p" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-2700.pileup.p.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-2700.pileup.bed.gz | awk '{if ($4=="m" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-2700.pileup.m.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-2700.pileup.bed.gz | awk '{if ($4=="i" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-2700.pileup.i.cov4_pmod10_mod2_omod0.bed
+gzcat 7316-3510.pileup.bed.gz | awk '{if ($4=="a" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-3510.pileup.a.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-3510.pileup.bed.gz | awk '{if ($4=="p" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-3510.pileup.p.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-3510.pileup.bed.gz | awk '{if ($4=="m" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-3510.pileup.m.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-3510.pileup.bed.gz | awk '{if ($4=="i" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-3510.pileup.i.cov4_pmod10_mod2_omod0.bed
+gzcat 7316-438.pileup.bed.gz | awk '{if ($4=="a" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-438.pileup.a.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-438.pileup.bed.gz | awk '{if ($4=="p" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-438.pileup.p.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-438.pileup.bed.gz | awk '{if ($4=="m" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-438.pileup.m.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-438.pileup.bed.gz | awk '{if ($4=="i" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-438.pileup.i.cov4_pmod10_mod2_omod0.bed
+gzcat 7316-489.pileup.bed.gz | awk '{if ($4=="a" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-489.pileup.a.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-489.pileup.bed.gz | awk '{if ($4=="p" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-489.pileup.p.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-489.pileup.bed.gz | awk '{if ($4=="m" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-489.pileup.m.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-489.pileup.bed.gz | awk '{if ($4=="i" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-489.pileup.i.cov4_pmod10_mod2_omod0.bed
+gzcat 7316-670.pileup.bed.gz | awk '{if ($4=="a" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-670.pileup.a.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-670.pileup.bed.gz | awk '{if ($4=="p" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-670.pileup.p.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-670.pileup.bed.gz | awk '{if ($4=="m" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-670.pileup.m.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-670.pileup.bed.gz | awk '{if ($4=="i" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-670.pileup.i.cov4_pmod10_mod2_omod0.bed
+gzcat 7316-737.pileup.bed.gz | awk '{if ($4=="a" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-737.pileup.a.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-737.pileup.bed.gz | awk '{if ($4=="p" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-737.pileup.p.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-737.pileup.bed.gz | awk '{if ($4=="m" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-737.pileup.m.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-737.pileup.bed.gz | awk '{if ($4=="i" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-737.pileup.i.cov4_pmod10_mod2_omod0.bed
+gzcat 7316-906.pileup.bed.gz | awk '{if ($4=="a" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-906.pileup.a.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-906.pileup.bed.gz | awk '{if ($4=="p" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-906.pileup.p.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-906.pileup.bed.gz | awk '{if ($4=="m" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-906.pileup.m.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-906.pileup.bed.gz | awk '{if ($4=="i" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-906.pileup.i.cov4_pmod10_mod2_omod0.bed
+gzcat 7316-922.pileup.bed.gz | awk '{if ($4=="a" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-922.pileup.a.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-922.pileup.bed.gz | awk '{if ($4=="p" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-922.pileup.p.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-922.pileup.bed.gz | awk '{if ($4=="m" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-922.pileup.m.cov4_pmod10_mod2_omod0.bed &
+gzcat 7316-922.pileup.bed.gz | awk '{if ($4=="i" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > 7316-922.pileup.i.cov4_pmod10_mod2_omod0.bed
+gzcat N_4390.pileup.bed.gz | awk '{if ($4=="a" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > N_4390.pileup.a.cov4_pmod10_mod2_omod0.bed &
+gzcat N_4390.pileup.bed.gz | awk '{if ($4=="p" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > N_4390.pileup.p.cov4_pmod10_mod2_omod0.bed &
+gzcat N_4390.pileup.bed.gz | awk '{if ($4=="m" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > N_4390.pileup.m.cov4_pmod10_mod2_omod0.bed &
+gzcat N_4390.pileup.bed.gz | awk '{if ($4=="i" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > N_4390.pileup.i.cov4_pmod10_mod2_omod0.bed
+gzcat N_6321.pileup.bed.gz | awk '{if ($4=="a" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > N_6321.pileup.a.cov4_pmod10_mod2_omod0.bed &
+gzcat N_6321.pileup.bed.gz | awk '{if ($4=="p" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > N_6321.pileup.p.cov4_pmod10_mod2_omod0.bed &
+gzcat N_6321.pileup.bed.gz | awk '{if ($4=="m" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > N_6321.pileup.m.cov4_pmod10_mod2_omod0.bed &
+gzcat N_6321.pileup.bed.gz | awk '{if ($4=="i" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > N_6321.pileup.i.cov4_pmod10_mod2_omod0.bed
+gzcat N_8009.pileup.bed.gz | awk '{if ($4=="a" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > N_8009.pileup.a.cov4_pmod10_mod2_omod0.bed &
+gzcat N_8009.pileup.bed.gz | awk '{if ($4=="p" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > N_8009.pileup.p.cov4_pmod10_mod2_omod0.bed &
+gzcat N_8009.pileup.bed.gz | awk '{if ($4=="m" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > N_8009.pileup.m.cov4_pmod10_mod2_omod0.bed &
+gzcat N_8009.pileup.bed.gz | awk '{if ($4=="i" && $11>10 && $10 > 4 && $12 > 2 && $14==0) {print}}' > N_8009.pileup.i.cov4_pmod10_mod2_omod0.bed
